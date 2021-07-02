d44a7c5bc3672b6fbd49e4f3b3ec59b8b30ecd55
Clayman <Clayman@DESKTOP-DM8E77U>
2021-07-02 21:07:18 +0300

    Regex.

---
+++ Inheritance/RegularExpressions/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<regex>
+
+using namespace std;
+
+#define NAME_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::string last_name = "Tupenko";
+	std::string first_name = "Vasya";
+	//cout << "¬ведите фамилию, им€: "; cin >> last_name >> first_name;
+	std::regex rgx("[A-Z][a-z]{1,15}");	//regex - class, rgx - object;
+	if (std::regex_match(last_name.c_str(), rgx/*, std::regex_constants::match_any*/))
+	{
+		cout << "Surname OK:" << last_name << endl;
+	}
+	else
+	{
+		cout << "Bad surname!!!" << endl;
+	}
+
+	if (std::regex_match(first_name, rgx))
+	{
+		cout << "Name OK:" << first_name << endl;
+	}
+	else
+	{
+		cout << "Bad name!!!" << endl;
+	}
+}
